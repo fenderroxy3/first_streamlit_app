e1a47a2031c33b55c2b777e9e553eb6620997df9
fenderroxy3 <150257819+fenderroxy3@users.noreply.github.com>
2023-11-09 08:31:24 -0600

    Update streamlit_app.py
    added text box for user selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,15 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
+#New Section to display customer selection
+fruit=selection = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thank you for addding ', fruit_selection)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_selection)
+
+#take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_selection.json())
+streamlit.dataframe(fruityvice_normalized)
+
 
